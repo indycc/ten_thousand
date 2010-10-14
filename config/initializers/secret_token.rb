59a8ce05a7e3cf3ecafa1f8bb1cdcabc461756be
Matt Burke <spraints@gmail.com>
2010-10-14 07:31:47 -0400

    Created app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TenThousand::Application.config.secret_token = '09378f9a7d25f679ac57112021bb79efd8af8360d75fd13d8be39e6bc634102dd11486027b5af4a40003c5a5cbae30298965717b04a1fd8fdb60257c19f49daf'
