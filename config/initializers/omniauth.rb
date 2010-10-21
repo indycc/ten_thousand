e8bb296125ca73d438d0d61ee01412711e854423
Matt Burke <spraints@gmail.com>
2010-10-21 16:21:47 -0400

    Added a specific google openid auth provider.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,6 @@ require 'omniauth/openid'
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid # TODO -- http://github.com/cnicolaou/openid_active_record_store
+  provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+#  provider :twitter
 end
