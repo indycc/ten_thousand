f710ca5fc961e1c33c311d9c36e0c24792607024
Ball <myotherpants@gmail.com>
2010-11-04 08:02:35 -0400

    Refactor of practice_logs_controller time formatting

--- app/controllers/practice_logs_controller.rb
+++ app/controllers/practice_logs_controller.rb
@@ -43,13 +43,7 @@ class PracticeLogsController < ApplicationController
   # POST /practice_logs.xml
   def create
     @practice_log = PracticeLog.new(params[:practice_log])
-    time_formatted = params[:practice_log][:duration]
-    if ((not time_formatted.nil?) and time_formatted.match(":") )
-      match = time_formatted.match('(\d{1,2}):(\d{1,2})')
-      hours_part = match[1].to_i * 60
-      minutes_part = match[2].to_i
-      @practice_log.duration = hours_part + minutes_part
-    end
+    @practice_log.duration = fix_duration(params[:practice_log][:duration])
 
     respond_to do |format|
       if @practice_log.save
@@ -66,6 +60,8 @@ class PracticeLogsController < ApplicationController
   # PUT /practice_logs/1.xml
   def update
     @practice_log = PracticeLog.find(params[:id])
+    # TODO : 
+    #params[:practice_log][:duration] = fix_duration(params[:practice_log][:duration])
 
     respond_to do |format|
       if @practice_log.update_attributes(params[:practice_log])
@@ -87,6 +83,18 @@ class PracticeLogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(practice_logs_url) }
       format.xml  { head :ok }
+    end
+  end
+  
+  private
+  def fix_duration(time_formatted)
+    if ((not time_formatted.nil?) and time_formatted.match(":") )
+      match = time_formatted.match('(\d{1,2}):(\d{1,2})')
+      hours_part = match[1].to_i * 60
+      minutes_part = match[2].to_i
+      hours_part + minutes_part
+    else
+       time_formatted.to_i
     end
   end
 end
