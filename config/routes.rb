ca633d3c7816c3891b3e9d4810e41bd590eab2c2
Matt Burke <spraints@gmail.com>
2010-11-11 08:05:23 -0500

    Add a dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TenThousand::Application.routes.draw do
   resources :expertises
   resources :practice_logs
+  match '/home' => 'dashboard#index', :as => 'dashboard'
   root :to => "welcome#index"
 end
