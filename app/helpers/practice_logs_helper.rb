1e25861667223573122359332cd2669607dc5e58
Brian Ball <myotherpants@gmail.com>
2010-11-11 08:02:37 -0500

    Really Formatted out put time

--- app/helpers/practice_logs_helper.rb
+++ app/helpers/practice_logs_helper.rb
@@ -1,2 +1,15 @@
 module PracticeLogsHelper
+
+  def format_time(number)
+    ftime = ""
+     
+    hours = (number / 60).to_i
+    ftime = pluralize(hours, "hour") if hours > 0
+    number = number - (hours * 60)
+    ftime += " " if hours > 0 && number > 0 
+    ftime += pluralize(number, "minute") if number > 0
+    return ftime
+  end
+  
 end
+
