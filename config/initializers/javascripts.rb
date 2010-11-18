d9f0db950d6507a923bd040c6825d9252fb0825a
Matt Burke <spraints@gmail.com>
2010-11-18 07:51:49 -0500

    Replace prototype with jquery and add a datepicker for practice log entry.

---
+++ config/initializers/javascripts.rb
@@ -0,0 +1,5 @@
+TenThousand::Application.config.action_view.javascript_expansions[:default] = [
+      'jquery.min.js',
+      'rails.js',
+      'application.js'
+    ]
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $('.datepicker')
+    .datepicker()
+    .datepicker('option', 'dateFormat', 'yy-mm-dd')
+    .datepicker('setDate', new Date());
+});
