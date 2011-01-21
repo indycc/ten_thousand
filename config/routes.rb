676e4948498b337c871015d24198ff71ddde9a0b
Matt Burke <spraints@gmail.com>
2011-01-21 13:59:33 -0500

    Removed locale scope because it breaks omnisocial.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 TenThousand::Application.routes.draw do
-  scope '(:locale)' do
-    controller :reports do
-      get "reports/calendar(/:year(/:month))", :as => 'calendar_report', :action => 'calendar', :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
-    end
-
-    resources :expertises
-    resources :practice_logs
-    match '/home' => 'dashboard#index', :as => 'dashboard'
-    root :to => "welcome#index"
+  controller :reports do
+    get "reports/calendar(/:year(/:month))", :as => 'calendar_report', :action => 'calendar', :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
   end
+
+  resources :expertises
+  resources :practice_logs
+  match '/home' => 'dashboard#index', :as => 'dashboard'
+  root :to => "welcome#index"
 end
