8b2e40343d3b1601292a55dcd78fc7f29b16aca0
Matt Burke <spraints@gmail.com>
2011-01-21 13:05:42 -0500

    Enable i18n.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   protected
   def deny_access
     flash[:error] = 'You must be logged in to continue.'
     redirect_to root_path
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale]
+  end
+
+  def default_url_options
+    super.merge :locale => I18n.locale
   end
 end
