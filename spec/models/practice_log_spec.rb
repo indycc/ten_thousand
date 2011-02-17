6905a59af2b1cc394e7f5ae09db468cedf88998c
James Alexander <jamesralexander@gmail.com>
2011-02-17 07:50:31 -0500

    Fixing tabs

--- spec/models/practice_log_spec.rb
+++ spec/models/practice_log_spec.rb
@@ -25,47 +25,47 @@ describe PracticeLog do
     subject.valid?.should be_false
     subject.errors[:duration].should == ["Duration cannot be zero"]
   end
-    context "With Hour" do
-      before (:each) do
-        @p_log.duration = 75.minutes
-      end
-      it "should display as 1:15" do
-        @p_log.practice_duration.should == "1:15"
-      end
-      it "should accept 1:30 as 90 minutes" do
-        @p_log.practice_duration = "1:30"
-        @p_log.duration.should == 90.minutes
-      end
+  context "With Hour" do
+    before (:each) do
+      @p_log.duration = 75.minutes
     end
+    it "should display as 1:15" do
+      @p_log.practice_duration.should == "1:15"
+    end
+    it "should accept 1:30 as 90 minutes" do
+      @p_log.practice_duration = "1:30"
+      @p_log.duration.should == 90.minutes
+    end
+  end
 
-    context "Without Hour" do
-      before(:each) do
-        @p_log.duration = 45.minutes
-      end
-      it "should display as 45" do
-        @p_log.practice_duration.should == "45"
-      end
-      it "should accept 45 as 45 minutes" do
-        @p_log.practice_duration = "45"
-        @p_log.duration.should == 45.minutes
-      end
+  context "Without Hour" do
+    before(:each) do
+      @p_log.duration = 45.minutes
     end
+    it "should display as 45" do
+      @p_log.practice_duration.should == "45"
+    end
+    it "should accept 45 as 45 minutes" do
+      @p_log.practice_duration = "45"
+      @p_log.duration.should == 45.minutes
+    end
+  end
 
-    context "Jerk move" do
-      before(:each) { @p_log.duration = 75.minutes }
-      it "should do bad things with bad input" do
-        @p_log.practice_duration = 'a'
-        @p_log.duration.should be_nil
-      end
-      it "should error when no duration is specified" do
-        @p_log.duration = 0
-        @p_log.practice_duration = ""
-        lambda { 
-          @p_log.save!
-        }.should raise_error
-        
-      end
+  context "Jerk move" do
+    before(:each) { @p_log.duration = 75.minutes }
+    it "should do bad things with bad input" do
+      @p_log.practice_duration = 'a'
+      @p_log.duration.should be_nil
     end
+    it "should error when no duration is specified" do
+      @p_log.duration = 0
+      @p_log.practice_duration = ""
+      lambda { 
+        @p_log.save!
+      }.should raise_error
+      
+    end
+  end
 
   it 'has event calendar' do
     # PracticeLog needs to do has_event_calendar. To ensure it's (mostly) working
